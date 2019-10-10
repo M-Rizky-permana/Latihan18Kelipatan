c1e179f2c1092bdbd3bb26e1c49cb6e2ee8188fc
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-10-11 00:20:39 +0700

    Add files via upload

---
+++ PBO210118050Latihan18Kelipatan/src/latihan18/Latihan18.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan18;
+
+/**
+ *
+ * NAMA :Muhamad Rizky Permana
+ * KELAS : IF-2
+ * NIM : 10118050
+ * Deskripsi Program : Program Kelipatan
+ */
+public class Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i = 3.5;
+        while (i<36){
+            System.out.println(i);
+            i = i + 3.5;
+        }
+        
+    }
+    
+}
